64b4181f14e5b9309746b1a4477cb620b225700c
Somiwoojoopapa1212 <shuttle1207@gmail.com>
2026-05-07 14:32:01 +0900

    feat: 우주봇 단위 연구소 MVP (길이/무게/용량/시간 행성, 문제 32개)

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'unit-lab-20260507000000';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (res && res.status === 200 && res.type !== 'opaque')
+        caches.open(CACHE_NAME).then(c => c.put(e.request, res.clone()));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
